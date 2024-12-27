6075fdc10a4a91015cbc5c31f200685751b9d3f6
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-12-27 18:51:05 +0600

    Add urls for categories

--- drf_first_app/urls.py
+++ drf_first_app/urls.py
@@ -1,7 +1,8 @@
 # urls.py
 from django.urls import path
-from .views import CurrencyConversionAPIView
+from .views import CurrencyConversionAPIView, CategoryListView
 
 urlpatterns = [
     path('currency-convert/', CurrencyConversionAPIView.as_view(), name='currency-convert'),
+    path('categories/', CategoryListView, name='category-list'),
 ]
