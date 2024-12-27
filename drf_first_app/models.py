4af1b926638011627f04de4b7f62e7bca0554c1c
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-12-27 18:40:54 +0600

    Create Category model

--- drf_first_app/models.py
+++ drf_first_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_code = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True, max_length=200) 
